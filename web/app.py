3d73761ccb6115f721d0ebe0df7fa88d1bfa8a28
ao-tanaka <119277988+ao-tanaka@users.noreply.github.com>
2023-01-09 09:38:37 +0900

    Update app.py

--- web/app.py
+++ web/app.py
@@ -11,6 +11,7 @@ def mainmenu():
     <body>
     <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
     <h2>Piper, CI/CD Lab11-3</h2>
+    <iframe src="https://calendar.google.com/calendar/embed?src=yoummywife%40gmail.com&ctz=Asia%2FTokyo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </body>
     </html>"""
 
