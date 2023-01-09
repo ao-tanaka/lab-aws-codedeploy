be01e3e0def307e6e2a47bf49922c7f2adf61f9a
ao-tanaka <119277988+ao-tanaka@users.noreply.github.com>
2023-01-09 21:37:36 +0900

    Update app.py

--- web/app.py
+++ web/app.py
@@ -9,8 +9,8 @@ def mainmenu():
     return """
     <html>
     <body>
-    <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
-    <h2>Piper, CI/CD Lab11-3</h2>
+    <center><h1>室温・湿度日記</h1><br/>
+    <h2>Piper</h2>
     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FTokyo&showTitle=0&showTz=0&showCalendars=1&showTabs=1&showDate=1&src=eW91bW15d2lmZUBnbWFpbC5jb20&src=ZjllZjBjNDlmYWMzMDYxN2NjNjNiZTBhMTM4NzgxOTAwNjI5OGM3NDdmMzUwY2EwNjUwZGZkNTIyNzlkNWVhNkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%239E69AF&color=%230B8043&color=%2333B679" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </body>
     </html>"""
